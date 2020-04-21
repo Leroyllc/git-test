c55201e763233ab7f6ceb2d1b0ccc97249457eb4
Leroy <294425948@qq.com>
2020-04-21 22:04:03 +0800

    把本地工程添加到git

---
+++ first_git_test/src/cn/llc/git/FirstClass.java
@@ -0,0 +1,16 @@
+package cn.llc.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+        System.out.println("hello git1");
+        System.out.println("hello git1");
+        System.out.println("hello git1");
+        System.out.println("hello git1");
+        System.out.println("hello git1");
+        System.out.println("hello git1");
+        System.out.println("hello git1");
+        System.out.println("hello git1");
+        System.out.println("hello git1");
+    }
+}
